8d34e323bf49a2ae28904df37e23188c3e9d0349
Alessio Serra <71273533+codiceSpaghetti@users.noreply.github.com>
2022-09-06 13:06:42 +0200

    Create install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,14 @@
+apt-get update
+/usr/bin/python3 -m pip install --upgrade pip
+pip install tensorflow==2.8
+apt install --allow-change-held-packages libcudnn8=8.1.0.77-1+cuda11.2
+pip install pandas
+pip install tensorflow_addons
+pip install opencv-python
+pip install pillow
+pip install vit_keras
+pip install faiss-cpu
+pip install -U scikit-learn
+pip install IPython
+pip install --upgrade tensorflow-hub
+pip -q install transformers
